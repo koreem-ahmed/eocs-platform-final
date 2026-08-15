09c18fac1c4388d548590e6510971a96c5c7d772
karim ahmed abd elkareem <karim.1024112@stemoctober.moe.edu.eg>
2026-08-15 21:15:39 +0300

    Prepare secure 2026 Vercel deployment

--- src/models/Submission.js
+++ src/models/Submission.js
@@ -10,7 +10,7 @@ const submissionSchema = new mongoose.Schema({
         type: Number,
         required: true,
         min: 1,
-        max: 19
+        max: 20
     },
     section: {
         type: String,
